9f873e36a08ff823e050adeb2e6ae584b5903962
Nathaniel Moschkin <precise.tester@gmail.com>
2024-01-28 23:31:32 -0500

    revert

--- src/beholdhelper.cpp
+++ src/beholdhelper.cpp
@@ -135,9 +135,9 @@ class Searcher
 		// group by image index
 		std::map<int, int> occurences;
 		for (const auto &match : matches) {		
-			if (_symbols[match.imgIdx] != "behold_title") {
+			//if (_symbols[match.imgIdx] != "behold_title") {
 				occurences[match.imgIdx]++;
-			}
+			//}
 		}
 
 		auto max = std::max_element(
