3b1f2565c88995a5fd30290b1b10e29a2cf7b334
Nathaniel Moschkin <precise.tester@gmail.com>
2024-02-07 11:35:24 -0500

    debugging

--- src/networkhelper.cpp
+++ src/networkhelper.cpp
@@ -119,9 +119,9 @@ bool NetworkHelper::downloadUrl(const std::string &url, std::function<bool(std::
 	auto uri = parseURI(url);
 	std::cout << "Request downloadUrl: " << url << "\r\n";
 	
-	std::string resource = uri.resource << "?" << uri.query;
+	std::string thisres = uri.resource << "?" << uri.query;
 
-	http::request<http::string_body> req{http::verb::get, uri.resource, 11};
+	http::request<http::string_body> req{http::verb::get, thisres, 11};
 
 	// Declare a container to hold the response
 	http::response<http::vector_body<uint8_t>> res;
